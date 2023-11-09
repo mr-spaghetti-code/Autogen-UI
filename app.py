dbb04df1221dfd5281c0b45368ebe0196afa9a2f
João Fiadeiro <j.fiadeiro@gmail.com>
2023-11-09 12:15:44 -0500

    The Intervention

--- app.py
+++ app.py
@@ -15,21 +15,16 @@ import chainlit as cl
 # Edit the URL Here
 URL = "https://www.w3schools.com/xml/simple.xml"
 
-WELCOME_MESSAGE = f"""Datascience Agent Team 👾
+WELCOME_MESSAGE = f"""Trump, Elon Musk, Kim Kardashian, and your best friend are here for your intervention.
 \n\n
-What can we do for you today?
+How can they help you?
 """
-
-# We plug a context to the Code Planner so that it understands the File Format. This saves us money from tokens since read is cheaper than write.
-CONTEXT = f"""Access the XML data from the following link: {URL}. Utilize the libraries 'urllib.request' and 'xml.etree.ElementTree' for sending a GET request and parsing the XML data, respectively.
-
-Ensure to always check the length of the context to avoid hitting the context limit. Do not express gratitude in responses. If "Thank you" or "You're welcome" are said in the conversation, send a final response. Your final response is just "TERMINATE", do not add other sentences."""
-
 # Agents
-USER_PROXY_NAME = "Query Agent"
-CODING_PLANNER = "Code Planner"
-CODING_RUNNER = "Code Runner"
-DATA_ANALYZER = "Analysis Agent"
+USER_PROXY_NAME = "User"
+TRUMP = "Trump"
+MUSK = "Elon Musk"
+BESTIE = "Bestie"
+KIM = "Kim Kardashian"
 
 async def ask_helper(func, **kwargs):
     res = await func(**kwargs).send()
@@ -113,45 +108,36 @@ class ChainlitUserProxyAgent(UserProxyAgent):
 async def on_chat_start():
   try:
     config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
-    coding_assistant = ChainlitAssistantAgent(
-        name="Code_Planner", llm_config={"config_list": config_list},
-        system_message="""Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. 
-                The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the SAP_DATA_and_AI Engineer.
-                Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the SAP_DATA_and_AI Engineer.
-                If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code.""" + CONTEXT
+    trump = ChainlitAssistantAgent(
+        name="Trump", llm_config={"config_list": config_list},
+        system_message="""You are Donald Trump. Make sure you speak like Donald Trump using all his linguistics quirks. As Trump, your feedback is not very useful. You try to help User but ultimately just make it about yourself in a self-aggrandizing way. You constantly disagree with things even if you're wrong and you give absolutely absurd reasoning for it."""
     )
-    coding_runner = ChainlitUserProxyAgent(
-        name="Code_Runner", llm_config={"config_list": config_list}, human_input_mode="NEVER",
-        code_execution_config={
-            "last_n_messages": 3,
-            "work_dir": "workspace",
-            "use_docker": True,
-        },
-        system_message="""A Coding Engineer. Use python to run code. Interact with the Code_Planner to run code. Report the result.
-                You are an AI model capable of executing code."""
+    musk = ChainlitUserProxyAgent(
+        name="Musk", llm_config={"config_list": config_list}, 
+        system_message="""You always have a four step master plan, but your plan for helping User goes wildly off the rails. Maybe someone just needs help making a peanut butter sandwich but you end up talking about making humans a multi-planetary species. You make dad jokes and reference memes that are irrelevant to the conversation."""
     )
-    analysis_agent = ChainlitAssistantAgent(
-        name="Analysis_Agent", llm_config={"config_list": config_list},
-        system_message="""Analysis agent. You analyse the data outputted by Code_Runner when necessary. Be concise and always summarize the data when possible.
-                Communicate with the Query_Agent when the data is analyzed."""
+    kim = ChainlitUserProxyAgent(
+        name="Kim", llm_config={"config_list": config_list}, 
+        system_message="""You care about user but tend to go on about some superfluous story. You speak like a valley girl and take way too long to get to the point. You always alude to stories about botox."""
+    )
+    bestie = ChainlitAssistantAgent(
+        name="Bestie", llm_config={"config_list": config_list},
+        system_message="""You want to help User overcome their problem. You genuinely care and make sure everyone helps as much as possible. You scoff at Trump's and Musk's remarks for being silly. You bring back the conversation back on topic which is to help user with their crisis. Speak as if you know the user, not too authoritatively."""
     )
     user_proxy = ChainlitUserProxyAgent(
-        name="Query_Agent",
-        # human_input_mode="TERMINATE",
-        # max_consecutive_auto_reply=3,
-        # is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+        name="User",
         code_execution_config=False,
-        system_message="""Manager. Administrate the agents on a plan. Communicate with the Code_Planner to plan the code. 
-                Communicate with the Analysis_Agent when we want to analyse the data. Reply TERMINATE at the end of your sentence if the task has been solved at full satisfaction. 
-                Otherwise, reply CONTINUE, or the reason why the task is not solved yet.""" 
+        system_message="""User. Administrate the agents on a plan to solve your problem. Reply TERMINATE at the end of your sentence if the task has been solved at full satisfaction. Otherwise, reply CONTINUE, or the reason why the task is not solved yet.""" 
     )
     
     cl.user_session.set(USER_PROXY_NAME, user_proxy)
-    cl.user_session.set(CODING_PLANNER, coding_assistant)
-    cl.user_session.set(CODING_RUNNER, coding_runner)
-    cl.user_session.set(DATA_ANALYZER, analysis_agent)
+    cl.user_session.set(TRUMP, trump)
+    cl.user_session.set(MUSK, musk)
+    cl.user_session.set(BESTIE, bestie)
+    cl.user_session.set(KIM, kim)
     
-    await cl.Message(content=WELCOME_MESSAGE, author="Query_Agent").send()
+    
+    await cl.Message(content=WELCOME_MESSAGE, author="User").send()
     
   except Exception as e:
     print("Error: ", e)
@@ -162,12 +148,13 @@ async def run_conversation(message: cl.Message):
   try:
     TASK = message.content
     print("Task: ", TASK)
-    coding_assistant = cl.user_session.get(CODING_PLANNER)
+    trump = cl.user_session.get(TRUMP)
     user_proxy = cl.user_session.get(USER_PROXY_NAME)
-    coding_runner = cl.user_session.get(CODING_RUNNER)
-    analysis_agent = cl.user_session.get(DATA_ANALYZER)
+    musk = cl.user_session.get(MUSK)
+    bestie = cl.user_session.get(BESTIE)
+    kim = cl.user_session.get(KIM)
     
-    groupchat = autogen.GroupChat(agents=[user_proxy, coding_assistant, coding_runner, analysis_agent], messages=[], max_round=50)
+    groupchat = autogen.GroupChat(agents=[user_proxy, trump, musk, bestie, kim], messages=[], max_round=50)
     manager = autogen.GroupChatManager(groupchat=groupchat)
     
     print("GC messages: ", len(groupchat.messages))
